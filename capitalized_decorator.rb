a6f21626f346200adacfc3700a05e9d15d4251c1
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-23 10:17:21 +0200

    Create a class that inherits from the base Decorator class

--- capitalized_decorator.rb
+++ capitalized_decorator.rb
@@ -1,6 +1,5 @@
 class CapitalizedDecorator < Nameable
-    def correct_name
-      correct_name.capitalize
-    end
+  def correct_name
+    correct_name.capitalize
   end
-  
+end
--- trim_decorator.rb
+++ trim_decorator.rb
@@ -1,7 +1,6 @@
 class TrimmerDecorator < Decorator
-    def correct_name
-      name = @nameable.correct_name
-      name.length > 10 ? name[0...10] : name
-    end
+  def correct_name
+    name = @nameable.correct_name
+    name.length > 10 ? name[0...10] : name
   end
-  
+end
