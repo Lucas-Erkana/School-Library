0d1802aeaa54e3dcfef6cb25f5aa3c71f537b925
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-25 10:40:09 +0200

    Define the entry point in main.rb, this will be a method called main that is invoked at the end of your file

--- main.rb
+++ main.rb
@@ -1,12 +1,51 @@
-require_relative 'person'
-require_relative 'capitalized_decorator'
-require_relative 'trim_decorator'
+require './app'
 
-person = Person.new(22, name: 'maximilianus')
-puts "Original name: #{person.correct_name}"
+class Main
+  def initialize
+    @app = App.new
+  end
 
-capitalized_person = CapitalizedDecorator.new(person)
-puts "Capitalized name: #{capitalized_person.correct_name}"
+  def run
+    puts 'Welcome to School Library App! '
+    loop do
+      puts 'Please choose an option by entering a number :'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
+      option = gets.chomp
 
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts "Capitalized and trimmed name: #{capitalized_trimmed_person.correct_name}"
+      case option
+      when '1'
+        @app.list_books
+      when '2'
+        @app.list_people
+      when '3'
+        @app.create_person
+      when '4'
+        @app.create_book
+      when '5'
+        @app.create_rental
+      when '6'
+        @app.list_rentals
+      when '7'
+        break
+      else
+        puts 'That is not a valid number'
+      end
+
+      puts ' '
+
+      puts 'Press any key to continue '
+      gets.chomp
+    end
+
+    puts 'Thank you !'
+  end
+end
+
+main = Main.new
+main.run
--- person.rb
+++ person.rb
@@ -3,10 +3,9 @@ require_relative 'nameable'
 class Person < Nameable
   attr_reader :id, :rentals
   attr_accessor :name, :age
-
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = rand(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -19,15 +18,16 @@ class Person < Nameable
     @age >= 18
   end
 
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
   def correct_name
     @name
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   def add_rental(book, date)
     Rental.new(date, book, self)
   end
+
 end
