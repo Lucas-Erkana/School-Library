e33ed2f47c2448bb83cf703ab1b753b3f18a595a
Lucas David Erkana <lderkana21x@gmail.com>
2023-05-30 06:50:34 +0200

    Update interface.rb

--- interface.rb
+++ interface.rb
@@ -1,84 +1,72 @@
-require './app'
+require './data_store'
 
-class Interface
-  OPTION_ACTIONS = {
-    '1' => :handle_list_books,
-    '2' => :handle_list_people,
-    '3' => :handle_create_person,
-    '4' => :handle_create_book,
-    '5' => :handle_create_rental,
-    '6' => :handle_list_rentals,
-    'q' => :quit
-  }.freeze
+def hello_message
+  app = App.new
+  puts ['Welcome to School Library App !', '']
+  app
+end
 
-  def initialize(app)
-    @app = app
+def create_person
+  print 'Do you want to create a student (1) or a teacher (2)? '
+  print '[Input the number ]: '
+end
+
+def display_options
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts 'Q - Quit'
+end
+
+def checking_options(app)
+  number = nil
+  until number == 'Q'
+    display_options
+    input = gets.chomp
+    puts 'This option is invalid, please select a number from [1..7]' if input < '1' || input > '7'
+    process_data(input, app)
   end
+end
 
-  def start
-    puts 'Welcome to School Library App!'
-    loop do
-      display_options
-      option = gets.chomp
-
-      handle_option(option.downcase)
-
-      puts ' '
-      break if option.downcase == 'q'
-    end
-
-    puts 'Thank you! You are always welcome.'
+def person_option(option, app)
+  case option
+  when 1
+    app.handle_st_input
+  when 2
+    app.handle_tr_input
+  else
+    puts 'This option is invalid, please select a number from [1..2]'
+    option = gets.to_i
+    person_option(option, app)
   end
+end
 
-  private
-
-  def display_options
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts 'Q - Quit'
-  end
-
-  def handle_option(option)
-    action = OPTION_ACTIONS[option]
-    if action
-      send(action)
-    else
-      puts '*************************************' unless option.downcase == 'q'
-      puts 'That is not a valid number or option' unless option.downcase == 'q'
-      puts '*************************************' unless option.downcase == 'q'
-    end
-  end
-
-  def handle_list_books
-    @app.list_books
-  end
-
-  def handle_list_people
-    @app.list_people
-  end
-
-  def handle_create_person
-    @app.create_person
-  end
-
-  def handle_create_book
-    @app.create_book
-  end
-
-  def handle_create_rental
-    @app.create_rental
-  end
-
-  def handle_list_rentals
-    @app.list_rentals
-  end
-
-  def quit
-    # No action required for quitting
+def process_data(input, app)
+  case input
+  when '1'
+    app.list_books
+  when '2'
+    app.list_people
+    save_data('people_list', app.people_list)
+  when '3'
+    create_person
+    option = gets.to_i
+    person_option(option, app)
+  when '4'
+    app.create_book
+    save_data('books_list', app.book_list)
+  when '5'
+    app.create_rental
+    save_data('rentals_list', app.rentals_list)
+  when '6'
+    app.list_rentals
+  when 'Q'
+    puts 'Exiting program...'
+  else
+    puts 'This option is invalid, please select a number from [1..7]'
   end
 end
